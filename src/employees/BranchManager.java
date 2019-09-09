1776f5d0ade77e71591f1f23711a64230dcc6913
FROZENPRAWN <53968459+FROZENPRAWN@users.noreply.github.com>
2019-09-09 21:06:14 +1000

    Update BranchManager.java

--- src/employees/BranchManager.java
+++ src/employees/BranchManager.java
@@ -1,5 +1,16 @@
 package employees;
 
 public class BranchManager {
+  public BranchManager(String employeeID, String employStatus, double hourlyRate, double hours, HashMap<Integer, String> assignedPropertyList) {
+        super ( employeeID, employStatus, hourlyRate, hours, assignedPropertyList );
+    }
 
+    @Override
+    double calculateSalary() {
+        return getHourlyRate () * getHours ();
+    }
+    public void assignEmployee(SalesConsultant sc){
+        this.getAssignedPropertyList.put(sc.getEmployeeID(), propertyInformation);
+    }
+  
 }
