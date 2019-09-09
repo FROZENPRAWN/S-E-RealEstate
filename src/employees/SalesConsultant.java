b539984529ed5631b619e289a0db93894d439a93
FROZENPRAWN <53968459+FROZENPRAWN@users.noreply.github.com>
2019-09-10 09:42:17 +1000

    Update SalesConsultant.java

--- src/employees/SalesConsultant.java
+++ src/employees/SalesConsultant.java
@@ -1,7 +1,17 @@
 package employees;
 
-public class SalesConsultant extends Employee {
-	public SalesConsultant(String employeeId, String employeeStatus, Double hourlyRate, Double hours) {
-		super(employeeId, employeeStatus, hourlyRate, hours);
-	}
+import java.util.HashMap;
+
+public class SalesConsultant extends employee {
+    public SalesConsultant(String employeeID, String employStatus, double hourlyRate, double hours, HashMap<String, String> assignedPropertyList) {
+        super(employeeID, employStatus, hourlyRate, hours, assignedPropertyList);
+
+
+    }
+
+    @Override
+    double calculateSalary() {
+        return getHourlyRate()*getHours();
+    }
+
 }
